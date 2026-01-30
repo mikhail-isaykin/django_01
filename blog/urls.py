77edb69f105ffe430ae5615341a9a559762b45c2
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-01-30 23:43:22 +0300

    Add new views and urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from blog import views
 
 urlpatterns = [
     path('', views.index),
-    path('about/', views.about),
-    path('contact/', views.contact),
+    path('about/', views.about, kwargs={'name': 'Tom', 'age': 38}),
+    re_path(r'^contact/', views.contact),
+    path('user/<str:name>/<int:age>/', views.user),
 ]
- 
--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,18 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse('<h2>Главная</h2>')
 
-def about(request):
-    return HttpResponse('<h2>О сайте</h2>')
+def about(request, name, age):
+    return HttpResponse(f'''
+        <h2>О пользователе</h2>
+        <p>Имя: {name}</p>
+        <p>Возраст: {age}</p>
+        ''')
 
 def contact(request):
     return HttpResponse('<h2>Контакты</h2>')
+
+def user(request, name, age):
+    return HttpResponse(f'''
+        <h2>Имя: {name}</h2>
+        <h2>Возраст: {age}</h2>
+        ''')
