0cd4ce53e8a8942d7730e90737a54edf3d2b9df3
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-02-01 23:49:53 +0300

    Add static

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,5 @@ from django.shortcuts import render
 
 
 def index(request):
-    header = 'Данные пользователя'
-    langs = ['Python', 'Java', 'C#']
-    user = {'name': 'Tom', 'age': 230}
-    address = ('Абрикосовая', 23, 45)
-
-    data = {'header': header, 'langs': langs, 'user': user, 'address': address}
-    return render(request, 'blog/index.html', context=data)
+    cat = ['Python', 'Java', 'JS', 'Go', 'C#', 'Kotlin']
+    return render(request, 'blog/index.html', context={'cat': cat})
