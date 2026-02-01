d15ec02ac670c723150793ae4cb8a60f39ea2cd0
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-02-01 21:39:28 +0300

    Rework templates/blog/index.html

--- blog/views.py
+++ blog/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    header = 'Данные пользователя'
+    langs = ['Python', 'Java', 'C#']
+    user = {'name': 'Tom', 'age': 230}
+    address = ('Абрикосовая', 23, 45)
+
+    data = {'header': header, 'langs': langs, 'user': user, 'address': address}
+    return render(request, 'blog/index.html', context=data)
