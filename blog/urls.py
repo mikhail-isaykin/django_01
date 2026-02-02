5f7e4bfb3162598775091bef7d091a1ee9512d47
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-02-03 02:42:38 +0300

    Add tests.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,27 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from blog import views
 
+
+app_name = 'blog'
+
 urlpatterns = [
-    path('', views.index),
-    path('about/', views.about),
+    path(
+        '',
+        views.index,
+        name='home'
+    ),
+    path(
+        'about/',
+        TemplateView.as_view(
+            template_name='blog/about.html',
+            extra_context={'site': 'mysite.com'}
+        ),
+        name='about'
+    ),
+    path(
+        'contact/',
+        views.contact,
+        name='contact'
+    ),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def index(request):
-    return render(request, 'base.html')
+    return render(request, 'blog/index.html', context={'site': 'mysite.com'})
 
 
-def about(request):
-    return render(request, 'blog/about.html')
+def contact(request):
+    return redirect('blog:about')
