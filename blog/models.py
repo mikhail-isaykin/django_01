5ae3053469845a28d6d35344b16ad4fe6d10daad
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-02-17 21:32:35 +0300

    Minor fixes models.py/Comment

--- blog/models.py
+++ blog/models.py
@@ -20,9 +20,7 @@ class Post(models.Model):
     author = models.ForeignKey(User,
                                on_delete=models.CASCADE,
                                related_name='blog_posts')
-
     body = models.TextField()
-
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -52,10 +50,10 @@ class Post(models.Model):
                              self.slug])
     
 
-    class Comment(models.Model):
+class Comment(models.Model):
     post = models.ForeignKey(Post,
-                             on_delete=models.CASCADE,
-                             related_name='comments')
+                            on_delete=models.CASCADE,
+                            related_name='comments')
     name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
@@ -63,11 +61,13 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
 
+
     class Meta:
         ordering = ['created']
         indexes = [
             models.Index(fields=['created']),
         ]
+
 
     def __str__(self):
         return f'Comment by {self.name} on {self.post}'
