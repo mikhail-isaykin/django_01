4a9f811b39d13af345a223db0eac94c777a845da
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-03-10 23:37:55 +0300

    modified: add logout.html

--- config/settings.py
+++ config/settings.py
@@ -135,4 +135,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
+#LOGOUT_REDIRECT_URL = '/'
